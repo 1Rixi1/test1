d3a676db3dd3d3da212980ae29c862a18caa38e5
Maksim <grigoriev.2021@yandex.ru>
2022-02-09 09:54:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+
+console.log('arr' + '- obj');
+
+console.log(4 + +'5');
+
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+
+
+const isChecked = false,
+      isClouse = false;
+
+      console.log(isChecked || !isClouse);
